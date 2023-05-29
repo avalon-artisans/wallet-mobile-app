3755c397c5a239e1c7386d04ee25da64ca6cbf91
Daniel Sioson <git@siosond.com>
2023-05-30 01:26:18 +0800

    feat(screens): improve login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:wallet_mobile_app/widgets/elevated_button_widget.dart';
+import 'package:wallet_mobile_app/widgets/outlined_button_widget.dart';
 import 'package:wallet_mobile_app/widgets/text_field_widget.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -13,84 +15,69 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 16),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Text(
-                              tr('login'),
-                              style: const TextStyle(
-                                fontSize: 28,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          Text(tr('please_sign_in_to_continue')),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      child: TextFieldWidget(
-                        autofillHints: const [AutofillHints.email],
-                        label: tr('your_email'),
-                        placeholder: tr('email_address'),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      child: TextFieldWidget(
-                        autofillHints: const [AutofillHints.password],
-                        label: tr('your_password'),
-                        obscureText: true,
-                        placeholder: tr('password'),
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 25, 86, 219),
-                        minimumSize: const Size.fromHeight(36),
-                      ),
-                      child: Text(
-                        tr('login'),
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('${tr('don\'t_have_an_account')} '),
-                  InkWell(
-                    onTap: () {},
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: Text(
-                      tr('sign_up'),
+                      tr('login'),
                       style: const TextStyle(
-                        decoration: TextDecoration.underline,
-                        color: Colors.blue,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
+                  Text(
+                    tr('login_with_your_credentials'),
+                    style: const TextStyle(
+                      color: Colors.black54,
+                    ),
+                  ),
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: TextFieldWidget(
+                autofillHints: const [AutofillHints.email],
+                label: tr('your_email'),
+                placeholder: tr('email_address'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: TextFieldWidget(
+                autofillHints: const [AutofillHints.password],
+                label: tr('your_password'),
+                obscureText: true,
+                placeholder: tr('password'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: ElevatedButtonWidget(
+                text: tr('continue_upper_case'),
+                onPressed: () {},
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8),
+              child: OutlinedButtonWidget(
+                text: tr('sign_up_upper_case'),
+                onPressed: () {},
+              ),
+            ),
+          ],
         ),
       ),
     );
