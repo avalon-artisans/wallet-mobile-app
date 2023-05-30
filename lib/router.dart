7a59e82a01ebf87df42f31a24f83d65668006b39
Daniel Sioson <git@siosond.com>
2023-05-31 01:36:19 +0800

    feat(router): add router

---
+++ lib/router.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:wallet_mobile_app/screens/login_screen.dart';
+import 'package:wallet_mobile_app/screens/register_screen.dart';
+
+final GoRouter router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const LoginScreen();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'register',
+          builder: (BuildContext context, GoRouterState state) {
+            return const RegisterScreen();
+          },
+        ),
+      ],
+    ),
+  ],
+);
