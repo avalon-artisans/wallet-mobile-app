80300b4beee0ed3f2b8240aaaad7c3840cbfd3d2
Daniel Sioson <git@siosond.com>
2023-05-31 01:36:50 +0800

    feat(app): configure router

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,17 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:wallet_mobile_app/screens/login_screen.dart';
+import 'package:wallet_mobile_app/router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const LoginScreen(),
+    return MaterialApp.router(
       locale: context.locale,
       localizationsDelegates: context.localizationDelegates,
+      routerConfig: router,
       supportedLocales: context.supportedLocales,
       title: 'Flutter Demo',
       theme: ThemeData(
