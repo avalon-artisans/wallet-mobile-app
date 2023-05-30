51b191d13629592ecbd662e4156360bc9bd9a1d2
Daniel Sioson <git@siosond.com>
2023-05-31 00:30:57 +0800

    feat(main): lock orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:wallet_mobile_app/constants/locales.dart';
 import 'package:wallet_mobile_app/constants/paths.dart';
@@ -8,6 +9,9 @@ import 'package:wallet_mobile_app/screens/login_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+
+  // For now, only portrait device orientation is supported.
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   runApp(
     EasyLocalization(
